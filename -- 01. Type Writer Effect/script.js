ebb4b3a57f2cbf48ba2cfb324de1f18f0354dc07
mahmoud fouad <mfapolice@gmail.com>
2022-07-25 01:17:44 +0200

    done

--- -- 01. Type Writer Effect/script.js
+++ -- 01. Type Writer Effect/script.js
@@ -1,2 +1,19 @@
 'use strict';
-console.log('hi there');
+
+const words = ['Designer', 'Developer', 'Officer', 'Creator'];
+let i = 0;
+let count = 0;
+let letter = '';
+let current = '';
+
+(function looper() {
+  if (count === words.length) count = 0;
+  current = words[count];
+  letter = current.slice(0, i++);
+  document.querySelector('.text-type').textContent = letter;
+  if (letter.length === current.length) {
+    count++;
+    i = 0;
+  }
+  setTimeout(looper, 300);
+})();
