621f56e7b606432c4658b967513cd01d7bef1aec
mahmoud fouad <mfapolice@gmail.com>
2022-07-25 20:55:53 +0200

    update

--- -- 03. Counter Animation/script.js
+++ -- 03. Counter Animation/script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+// const counter = document.querySelectorAll('.counter');
+
+// counter.forEach(count => {
+//   count.textContent = '0';
+
+//   function updateCounter() {
+//     const target = +count.dataset.target;
+//     console.log(target);
+//     let c = +count.textContent;
+//     let increment = target / 250;
+//     if (c < target) {
+//       count.textContent = Math.ceil(c + increment);
+//       setTimeout(updateCounter, 1);
+//     }
+
+//     console.log(typeof target, target);
+//   }
+//   updateCounter();
+// });
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+document.querySelectorAll('.counter').forEach(count => {
+  console.log(count);
+  count.textContent = 0;
+  function updateUI() {
+    const target = +count.dataset.target;
+    let textUI = +count.textContent;
+    let i = Math.floor(target / 250);
+    if (textUI < target) {
+      count.textContent = i +
+    }
+  }
+  updateUI();
+});
