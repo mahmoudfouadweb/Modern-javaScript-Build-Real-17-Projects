32aa9610829cdaf649c23f7c863aada533c32658
mahmoud fouad <mfapolice@gmail.com>
2022-07-26 20:19:03 +0200

    done

--- -- 02. Responsive Image Gallery/script.js
+++ -- 02. Responsive Image Gallery/script.js
@@ -1,2 +1,28 @@
 'use strict';
-console.log('hi there');
+const container = document.querySelector('.container');
+const panel = document.querySelectorAll('.panel');
+
+// panel.forEach(img => {
+//   img.addEventListener('click', e => {
+//     console.log(e.target);
+//     const target = e.target;
+
+//     if (target) img.classList.add('active');
+//     if (!target) img.classList.remove('active');
+//   });
+// });
+
+container.addEventListener('click', e => {
+  const img = e.target;
+  // remove Active
+  img
+    .closest('.container')
+    .querySelectorAll('.panel')
+    .forEach(el => {
+      el.classList.remove('active');
+    });
+  if (!img) return;
+  if (img) {
+    img.classList.add('active');
+  }
+});
