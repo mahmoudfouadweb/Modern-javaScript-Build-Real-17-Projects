39b816e4b3e3f149f0ca668fa87de4cc393b3da9
mahmoud fouad <mfapolice@gmail.com>
2022-07-27 20:07:54 +0200

    init JS

--- -- 05. Music Player/script.js
+++ -- 05. Music Player/script.js
@@ -1,13 +1,13 @@
 'use strict';
 console.log('hi there');
 const musicContainer = document.querySelector('.music-container ');
-const musicContainer = document.querySelector('.music-container ');
-const musicContainer = document.querySelector('.music-container ');
-const musicContainer = document.querySelector('.music-container ');
-const musicContainer = document.querySelector('.music-container ');
-const musicContainer = document.querySelector('.music-container ');
-const musicContainer = document.querySelector('.music-container ');
-const musicContainer = document.querySelector('.music-container ');
-const musicContainer = document.querySelector('.music-container ');
-const musicContainer = document.querySelector('.music-container ');
-const musicContainer = document.querySelector('.music-container ');
+
+const play = document.querySelector('.play');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+const audio = document.querySelector('audio');
+const progressContainer = document.querySelector('.progress-container');
+const progress = document.querySelector('.progress');
+const title = document.querySelector('.title');
+const imgCover = document.querySelector('.img-cover');
