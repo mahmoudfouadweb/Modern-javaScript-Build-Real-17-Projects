28c5a4e255e197e4af3fb57477d6899c0e76dac4
mahmoud fouad <mfapolice@gmail.com>
2022-07-31 13:19:35 +0200

    update

--- -- 06 Build Step Progress Bar/script.js
+++ -- 06 Build Step Progress Bar/script.js
@@ -1,2 +1,49 @@
 'use strict';
-console.log('hi there');
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+const progress = document.querySelector('.progress');
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
+const circles = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+///////////////////////////////////////////////////
+document.addEventListener('DOMContentLoaded', update);
+///////////////////////////////////////////////////
+next.addEventListener('click', e => {
+  currentActive++;
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  update();
+  // console.log(currentActive);
+});
+
+///////////////////////////////////////////////////
+prev.addEventListener('click', e => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  update();
+  // console.log(currentActive);
+});
+
+///////////////////////////////////////////////////
+function update() {
+  circles.forEach((circle, i) => {
+    console.log(circle, i);
+    if (i < currentActive) circle.classList.add('active');
+    else circle.classList.remove('active');
+    const actives = document.querySelectorAll('.active');
+    progress.style.width = `${(actives.length / circles.length) * 100}%`;
+    if (currentActive == 1 || currentActive < 1) prev.disabled = true;
+    else if (currentActive == circles.length) next.disabled = true;
+    else {
+      prev.disabled = false;
+      next.disabled = false;
+    }
+  });
+}
