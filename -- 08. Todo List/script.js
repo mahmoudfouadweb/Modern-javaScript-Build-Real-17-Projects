9d846ea6aa7f566c81720d20f7b1ae2c6de8e766
mahmoud fouad <mfapolice@gmail.com>
2022-08-07 14:25:48 +0200

    update

--- -- 08. Todo List/script.js
+++ -- 08. Todo List/script.js
@@ -3,4 +3,47 @@
 const form = document.querySelector('.form');
 const input = document.querySelector('.input');
 const todos = document.querySelector('.todos');
-const demo = document.querySelector('.');
+
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+
+function addTodo() {
+  let userText = input.value;
+  const htmlTag = document.createElement('li');
+  htmlTag.textContent = htmlTag;
+  todos.insertAdjacentHTML('beforeend', htmlTag);
+  form.reset();
+}
+
+// function update() {
+//   todos.querySelectorAll('li').forEach(el => {
+//     el.addEventListener('click', e => {
+//       const clicked = e.target;
+//       if (clicked && clicked.classList.contains('completed')) {
+//         clicked.classList.remove('completed');
+//       } else {
+//         clicked.classList.add('completed');
+//       }
+//     });
+//     el.addEventListener('contextmenu', e => {
+//       e.preventDefault();
+//       const clicked = e.target;
+//       if (clicked) {
+//         clicked.style.display = 'none';
+//       }
+//       console.log(e.target);
+//     });
+//   });
+// }
+
+// form.addEventListener('submit', e => {
+//   e.preventDefault();
+//   addTodo();
+//   update();
+// });
