236aeb628a5a376c926684858ebb71ba9c1270ba
mahmoud fouad <mfapolice@gmail.com>
2022-08-08 14:33:55 +0200

    done finally

--- -- 08. Todo List/script.js
+++ -- 08. Todo List/script.js
@@ -1,14 +1,67 @@
 'use strict';
 
+///////////////////////////////////////////////
+// Setting General Variables
 const form = document.querySelector('.form');
 const input = document.querySelector('.input');
 const todosUl = document.querySelector('.todos');
 
 ///////////////////////////////////////////////
-///////////////////////////////////////////////
-
-///////////////////////////////////////////////
-
+// Update User Interface
 function updateUI() {
-  const Els = document.querySelectorAll('li');
+  let els = document.querySelectorAll('li');
+  const todos = [];
+  els.forEach(el => {
+    todos.push({
+      text: el.textContent,
+      done: el.classList.contains('completed'),
+    });
+  });
+  localStorage.setItem('todos', JSON.stringify(todos));
 }
+
+///////////////////////////////////////////////
+// Get Saved Todos
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if (todos)
+  todos.forEach(todo => {
+    addTodo(todo);
+  });
+
+///////////////////////////////////////////////
+// Todo Factory Good Luck🤞
+function addTodo(todo) {
+  let text = input.value;
+  if (todo) text = todo.text;
+  if (text) {
+    // create new Todo
+    const li = document.createElement('li');
+    if (todo && todo.done) li.classList.add('completed');
+    li.textContent = text;
+
+    // Delete a Completed Todo
+    li.addEventListener('contextmenu', e => {
+      e.preventDefault();
+      e.target.remove();
+      updateUI();
+    });
+    // Mark a Completed Todo
+    li.addEventListener('click', e => {
+      e.target.classList.toggle('completed');
+      updateUI();
+    });
+    //Add todo to User Interface
+    todosUl.insertAdjacentElement('beforeend', li);
+    // Clear Input Field
+    input.value = '';
+    updateUI();
+  }
+}
+
+///////////////////////////////////////////////
+// Form Control
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  addTodo();
+});
