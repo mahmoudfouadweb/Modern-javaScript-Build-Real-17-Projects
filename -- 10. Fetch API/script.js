31606616d037dc4d28d784d647c6fb4d114db893
mahmoud fouad <mfapolice@gmail.com>
2022-08-12 03:09:21 +0200

    update

--- -- 10. Fetch API/script.js
+++ -- 10. Fetch API/script.js
@@ -1,5 +1,23 @@
 'use strict';
+// const text = `mahmoud fouad  waffle iron 80  2
+// mahmoud fouad  blenfer 200 1
+// mahmoud fouad  knife 10  4
+// nikita smith  waffle iron 80  1
+// nikita smith  blenfer 10 2
+// nikita smith  knife 20  3`;
 
+// text
+//   .split('\n')
+//   .map(line => line.split('\t'))
+//   .reduce((customers, line) => {
+//     console.log(customers, line);
+//     customers[line[0]] = [];
+//     customers[line[0]].push({
+//       name: [line[0]],
+//     });
+//     return customers;
+//   }, {});
+console.log(text);
 let orders = [
   { amount: 250 },
   { amount: 400 },
